8a3656dab145c220db6c5cdb4a54d94003f6e02c
Lauren Hansbury <laurenhansbury@gmail.com>
2024-01-28 13:45:36 -0500

    added modal styles and basic table to modal

--- functions.js
+++
@@ -1,9 +0,0 @@
-const bedSelected = document.getElementbyID("bed-count-dropdown").value;
-
-bedSelected.addEventListener("click", (e) => {
-  if (e.target.value === "1 bedroom") {
-    console.log("1 bedroom");
-  } else {
-    console.log("nothing selected");
-  }
-});
